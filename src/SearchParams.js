0b71301658d533fd80e196577622a654184341fb
Rolandas1369 <rolandaswb@gmail.com>
2020-11-22 00:17:12 +0200

    add map hint

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,15 @@
+import { ANIMALS } from "@frontendmasters/pet";
 import React, { useState } from "react";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
-          location{" "}
+          Location
           <input
             id="location"
             value={location}
@@ -16,6 +18,22 @@ const SearchParams = () => {
           />
         </label>
       </form>
+      <label htmlFor="animal">Animal</label>
+      <select
+        name=""
+        value={animal}
+        id="animal"
+        onChange={(e) => setAnimal(e.target.value)}
+        onBlur={(e) => setAnimal(e.target.value)}
+      >
+        <option>All</option>
+        {ANIMALS.map((animal) => (
+          <option key={animal} value={animal}>
+            {animal}
+          </option>
+        ))}
+      </select>
+
       <button>Submit</button>
     </div>
   );
