a7d8f8f1a096d0a2a1d83179e3183432971822ce
Rolandas1369 <rolandaswb@gmail.com>
2020-11-29 21:07:53 +0200

    modal window implementaton, useRef exapmle

--- src/Modal.js
+++ src/Modal.js
@@ -4,7 +4,7 @@ import { createPortal } from "react-dom";
 const Modal = ({ children }) => {
   // useRef reference to one element and after this job it is destroid
   const elRef = useRef(null);
-  const div = document.createElement("div");
+
   if (!elRef.current) {
     const div = document.createElement("div");
     elRef.current = div;
